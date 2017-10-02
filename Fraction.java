6fda53dc2cf9d890df218a536c05ab0c584c3a4f
Utilisateur <user@debian.arcahe.ovh>
2017-10-02 15:52:25 +0200

    le constructeur et la fonction toStiong

--- Fraction.java
+++ Fraction.java
@@ -1,5 +1,19 @@
 public class Fraction{
+	
+	private int a;
+	private int b;
+	
+	public Fraction(int a, int b) {
+		this.a = a;
+		this.b = b;
+	}
+	
+	public String toString() {
+		return ("" + this.a+"/"+this.b);
+	}
+	
 	public static void main(String[] args) {
-		System.out.println("Bonjour!!!!");
+		Fraction f = new Fraction(5, 2);
+		System.out.println("La fraction de f est : " + f);
 	}
 }
